a1f4536cf8b8af9b089847877ac719a163641bc1
Александр Макаров <makarov.alxr@yandex.ru>
2020-04-21 13:51:52 +0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname, basename
+
+
+with open('README.md', 'r') as readme_file:
+    readme = readme_file.read()
+
+with open('requirements.txt', 'r') as req_file:
+    requirements = req_file.read().split('\n')
+
+
+setup_requirements = ["pip>=18.1"]
+
+authors = [
+    "N. Russkikh",
+    "A. Makarov",
+    "D. Antonets",
+    "D. Shtokalo"
+]
+
+setup(
+    author = authors,
+    author_email = "makarov.alxr@yandex.ru",
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Natural Language :: Russian",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    description = "Style transfer variational autoencoder",
+    name = "stVAE",
+    long_description = readme,
+    python_requires=">=3.6",
+    license = "MIT license",
+    packages = find_packages(),
+    setup_requires = setup_requirements,
+    version = "0.1",
+    url = "https://github.com/NRshka/stvae/source"
+)
